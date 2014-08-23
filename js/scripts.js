886147bc52d9f48e258c4d1c201d859ac6d24fc1
Roberto González <roberto@aerolab.com.ar>
2014-08-23 03:03:06 -0300

    JSConf Argentina 2014 - Rough Draft.

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+  var $calendar_buttons = $('.section-calendar .calendar-menu .button');
+  var $calendar_holder = $('.section-calendar .event-calendar-holder');
+  var $calendars = $calendar_holder.find('.event-calendar');
+
+  $calendar_buttons.click(function(event){
+    event.preventDefault();
+
+    var $button = $(this);
+    var day = $(this).data('day');
+    $calendar_buttons.not($button).removeClass('active');
+    $button.addClass('active');
+
+    var $calendar = $calendars.filter('[data-day="'+day+'"]');
+    var $old_calendars = $calendars.filter('.active').not($calendar);
+
+    // Sorry.
+    $old_calendars.addClass('active animate-left');
+    setTimeout(function(){
+      $old_calendars.removeClass('active animate-left');
+      $calendar.addClass('active animate-right');
+      setTimeout(function(){
+        $calendar.removeClass('animate-right');
+      }, 16);
+    }, 250);
+
+  });
+
+});
