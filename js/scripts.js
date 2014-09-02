b41d46d84af5a04b6593ed3beeabbdb41563adbe
RobertoGonzalez <roberto@aerolab.com.ar>
2014-09-02 16:25:46 -0300

    Added newsletter signup tracking for GA.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,7 @@
 $(document).ready(function(){
 
-  var $calendar_buttons = $('.section-calendar .calendar-menu .button');
-  var $calendar_holder = $('.section-calendar .event-calendar-holder');
-  var $calendars = $calendar_holder.find('.event-calendar');
-
-  $calendar_buttons.click(function(event){
-    event.preventDefault();
-
-    var $button = $(this);
-    var day = $(this).data('day');
-    $calendar_buttons.not($button).removeClass('active');
-    $button.addClass('active');
-
-    var $calendar = $calendars.filter('[data-day="'+day+'"]');
-    var $old_calendars = $calendars.filter('.active').not($calendar);
-
-    // Sorry.
-    $old_calendars.addClass('active animate-left');
-    setTimeout(function(){
-      $old_calendars.removeClass('active animate-left');
-      $calendar.addClass('active animate-right');
-      setTimeout(function(){
-        $calendar.removeClass('animate-right');
-      }, 16);
-    }, 250);
-
+  $('form.signup-form').submit(function(event){
+    ga( 'send', 'event', 'newsletter', 'signup' );
   });
 
 });
